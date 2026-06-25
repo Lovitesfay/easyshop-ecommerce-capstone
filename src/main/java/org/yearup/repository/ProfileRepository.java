8295ff88b75e814c6e065e132fc3b0d593731dad
lovitesfay <lovitesfay@gmail.com>
2026-06-24 21:03:47 -0500

    removed import principal from profile repo (never used) and create new profile controller class

--- src/main/java/org/yearup/repository/ProfileRepository.java
+++ src/main/java/org/yearup/repository/ProfileRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.yearup.models.Profile;
 
-import java.security.Principal;
-
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Integer>
 {
