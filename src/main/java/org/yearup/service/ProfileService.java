06cee20e1798f0b3163b7f86c76e97443afc40a2
lovitesfay <lovitesfay@gmail.com>
2026-06-24 20:54:28 -0500

    added get profile to service

--- src/main/java/org/yearup/service/ProfileService.java
+++ src/main/java/org/yearup/service/ProfileService.java
@@ -18,4 +18,9 @@ public class ProfileService
     {
         return profileRepository.save(profile);
     }
+    //// I added GET profile
+    public Profile getByUserId(int userId)
+    {
+        return profileRepository.findByUserId(userId);
+    }
 }
