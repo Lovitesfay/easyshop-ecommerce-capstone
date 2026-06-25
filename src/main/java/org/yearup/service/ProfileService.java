9f53c5178f47af9341e85a5e2df876bcc26670f8
lovitesfay <lovitesfay@gmail.com>
2026-06-24 20:58:51 -0500

    added update profile to ProfileService

--- src/main/java/org/yearup/service/ProfileService.java
+++ src/main/java/org/yearup/service/ProfileService.java
@@ -23,4 +23,21 @@ public class ProfileService
     {
         return profileRepository.findByUserId(userId);
     }
+
+    /// I added the update
+    public Profile update(int userId, Profile profile)
+    {
+        Profile existingProfile = profileRepository.findByUserId(userId);
+
+        existingProfile.setFirstName(profile.getFirstName());
+        existingProfile.setLastName(profile.getLastName());
+        existingProfile.setPhone(profile.getPhone());
+        existingProfile.setEmail(profile.getEmail());
+        existingProfile.setAddress(profile.getAddress());
+        existingProfile.setCity(profile.getCity());
+        existingProfile.setState(profile.getState());
+        existingProfile.setZip(profile.getZip());
+
+        return profileRepository.save(existingProfile);
+    }
 }
