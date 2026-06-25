c91c0c4795c418c73ec4379e0a8b7b998bec585f
lovitesfay <lovitesfay@gmail.com>
2026-06-24 21:10:24 -0500

    added private fields for profileService and UserService and created constructor for them

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -2,10 +2,21 @@ package org.yearup.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yearup.repository.ProfileRepository;
+import org.yearup.service.ProfileService;
+import org.yearup.service.UserService;
 
 @RestController
 @RequestMapping("/profile")
 public class ProfileController {
+
+    private final ProfileService profileService;
+    private final UserService userService;
+
+    public ProfileController(ProfileService profileService, UserService userService) {
+        this.profileService = profileService;
+        this.userService = userService;
+    }
 
 
 }
