1aab9db6a2beefe4c8d3c828d835a64f2de010ca
lovitesfay <lovitesfay@gmail.com>
2026-06-25 22:42:39 -0500

    made private final fields for OrderService and for UserService

--- src/main/java/org/yearup/controllers/OrderController.java
+++ src/main/java/org/yearup/controllers/OrderController.java
@@ -1,5 +1,9 @@
 package org.yearup.controllers;
 
+import org.yearup.service.UserService;
+
 public class OrderController {
 
+    private final OrderService orderService;
+    private final UserService userService;
 }
