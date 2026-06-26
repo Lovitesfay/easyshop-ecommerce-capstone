828d33a619a0963878d5c5ea4561b19e69b8be37
lovitesfay <lovitesfay@gmail.com>
2026-06-25 23:18:33 -0500

    created fields for Order Class

--- src/main/java/org/yearup/models/Order.java
+++ src/main/java/org/yearup/models/Order.java
@@ -1,7 +1,20 @@
 package org.yearup.models;
 
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Order {
+
+    /// fields
+    private int orderId;
+    private int userId;
+    private LocalDateTime date;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private BigDecimal shippingAmount;
 
 
 }
