6496765e8f8cf03425451881bbcaa48f3a8affd5
lovitesfay <lovitesfay@gmail.com>
2026-06-25 23:36:37 -0500

    added OrderlineItem to Checkout

--- src/main/java/org/yearup/service/OrderService.java
+++ src/main/java/org/yearup/service/OrderService.java
@@ -2,10 +2,12 @@ package org.yearup.service;
 
 import org.yearup.models.CartItem;
 import org.yearup.models.Order;
+import org.yearup.models.OrderLineItem;
 import org.yearup.models.Product;
 import org.yearup.repository.OrderRepository;
 import org.yearup.repository.ShoppingCartRepository;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -36,6 +38,13 @@ public class OrderService {
         for (CartItem cartItem : cartItems) {
             Product product = productService.getById(cartItem.getProductId());
 
+            OrderLineItem lineItem = new OrderLineItem();
+            lineItem.setOrderId(savedOrder.getOrderId());
+            lineItem.setProductId(cartItem.getProductId());
+            lineItem.setQuantity(cartItem.getQuantity());
+            lineItem.setSalesPrice(BigDecimal.getPrice);
+
+            orderLineItemRepository.save(lineItem);
 
         }
         shoppingCartRepository.deleteByUserId(userId);
